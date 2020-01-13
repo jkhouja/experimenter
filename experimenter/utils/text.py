1b17d5bf62d115da3d6ef7a95feb68c0e636e555
Jude Khouja <jude@latynt.com>
2020-01-13 10:38:48 -0800

    Updating docstring to match python3 style

--- experimenter/utils/text.py
+++ experimenter/utils/text.py
@@ -1,14 +1,15 @@
 import re
 
 def remove_diacritic(input_text: str) -> str:
-    """
-    Removes accents from arabic text
+    """Removes accents from arabic text
+
+    Calling this method on arabic text removes diacritics from the string.
 
     Args:
-        input_text (str): The text to be cleaned.
+        input_text: The text to be cleaned.
 
     Returns:
-        str: the text after removing all accents.
+        The text after removing all accents.
     """
     remove = re.compile(r"['ِ''ُ''ٓ''ٰ''ْ''ٌ''ٍ''ً''ّ''َ'`\"]")
     if input_text is None:
